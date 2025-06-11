edde10a8bc40727c3ee11d3e2f55c34946a810d7
techAmoghh <amoghhambarde123@gmail.com>
2025-06-11 14:59:51 +0530

    Landscape problem solved.

--- src/components/TypeAnimation.jsx
+++ src/components/TypeAnimation.jsx
@@ -9,10 +9,8 @@ export default function TypeAnimation({ greetings }) {
       transition={{ duration: 0.5 }}
       className="mb-0"
     >
-      <h1 className="text-sm sm:text-4xl md:text-lg  font-bold sm:w-52">
-        {/* <span className="block text-gray-800 dark:text-gray-200">
-          Hi, I'm Amogh
-        </span> */}
+      <h1 className="text-sm sm:text-sm md:text-lg  font-bold sm:w-52">
+        
         <span className=" text-gray-600 dark:text-gray-300">
           <Type
             sequence={[
@@ -34,12 +32,7 @@ export default function TypeAnimation({ greetings }) {
           />
         </span>
       </h1>
-      {/* <motion.div
-        initial={{ opacity: 0, x: -20 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ delay: 0.5, duration: 0.5 }}
-        className="h-0 w-10 sm:w-52 bg-gradient-to-r from-tech-blue to-tech-purple rounded-full"
-      /> */}
+      
     </motion.div>
   );
 }
